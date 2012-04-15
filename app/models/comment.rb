026720df1a15da6e1722893ac15cf425c180ae57
Ethermind <luis.capra@gmail.com>
2012-04-15 16:04:47 -0300

    add_comment_to_match fix, feedback fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :message, message: "message can't be blank"
   validates_length_of :message, :minimum => 2, too_short: "message should have at least 2 characters"
+  validates_presence_of :user, :on => :create, :message => "your session has been expired"
+  validates_presence_of :match, :on => :create, :message => "your session has been expired"
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,18 @@ Futbol::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :tls => true,
+    :address => 'smtp.gmail.com',
+    :port => "587",
+    :authentication => :plain,                                                                                                                     
+    :domain => 'test.com',
+    :user_name => 'user@test.com',
+    :password => 'mypass'
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
